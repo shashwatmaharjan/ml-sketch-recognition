ae147913bc971bf379a267c25e45b21023b0ab46
shashwatmaharjan <shashwatmjn@gmail.com>
2024-11-14 20:36:21 -0500

    Basic structure

---
+++ scripts/download_data.py
@@ -0,0 +1,15 @@
+# Import necessary libraries
+import os
+
+# Main function
+def main():
+
+    pass
+
+if __name__ == '__main__':
+
+    # Clear the screen regardless of the operating system
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    # Call the main function
+    main()
