57bab7ad55bb39a5d2a810223536f6c21d61c310
shashwatmaharjan <shashwatmjn@gmail.com>
2024-11-19 19:10:43 -0500

    Replace subcategory with subclass

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -14,14 +14,14 @@ def does_not_exist(folder, directory):
 
 
 # Function to return the folders that do not exist
-def return_folder_not_exist(subcategories, classes, directory):
+def return_folder_not_exist(subclasses, classes, directory):
 
     # Loop through all the categories
-    for subcategory in subcategories:
+    for subclass in subclasses:
 
         # If the category does not exist in the directory, print a message
-        if does_not_exist(subcategory, directory):
-            print(f'{subcategory} does not exist.')
+        if does_not_exist(subclass, directory):
+            print(f'{subclass} does not exist.')
     
     print(f'{classes} class check done!')
 
@@ -37,51 +37,51 @@ def main():
     # Create an empty dictionary labelled classes
     classes = {}
 
-    # Add the following subcategories to the vehicles class
-    category = 'vehicles'
-    classes[category] = ['bear', 'bee', 'butterfly', 'camel', 'cat', 'cow', 'crab', 'crocodile', 
+    # Add the following subclass to the vehicles class
+    subclass = 'vehicles'
+    classes[subclass] = ['bear', 'bee', 'butterfly', 'camel', 'cat', 'cow', 'crab', 'crocodile', 
                               'dog', 'dolphin', 'duck', 'elephant', 'fish', 'frog', 'giraffe', 'hedgehog', 
                               'kangaroo', 'lion', 'lobster', 'monkey', 'mouse', 'octopus', 'owl', 'panda', 
                               'parrot', 'penguin', 'pig', 'pigeon', 'rabbit', 'rooster', 'scorpion', 'shark', 
                               'sheep', 'snail', 'snake', 'spider', 'squirrel', 'swan', 'tiger', 'zebra']
     
-    # Check if all of these subcategories exist in the raw file directory
-    return_folder_not_exist(classes[category], category, images_data_directory)
+    # Check if all of these subclass exist in the raw file directory
+    return_folder_not_exist(classes[subclass], subclass, images_data_directory)
 
-    # Add the following subcategories to the animals class
-    category = 'animals'
-    classes[category] = ['airplane', 'blimp', 'bus', 'car', 'pickup truck', 'race car', 'suv', 
+    # Add the following subclass to the animals class
+    subclass = 'animals'
+    classes[subclass] = ['airplane', 'blimp', 'bus', 'car', 'pickup truck', 'race car', 'suv', 
                         'truck', 'van', 'bulldozer', 'bicycle', 'motorbike', 'ship', 'speed-boat', 
                         'canoe', 'sailboat', 'submarine', 'helicopter', 'space shuttle', 'train']
     
-    # Check if all of these subcategories exist in the raw file directory
-    return_folder_not_exist(classes[category], category, images_data_directory)
+    # Check if all of these subclass exist in the raw file directory
+    return_folder_not_exist(classes[subclass], subclass, images_data_directory)
     
-    # Add the following subcategories to the food class
-    category = 'food'
-    classes[category] = ['apple', 'banana', 'bread', 'cake', 'carrot', 'donut', 'grapes', 'hamburger', 
+    # Add the following subclass to the food class
+    subclass = 'food'
+    classes[subclass] = ['apple', 'banana', 'bread', 'cake', 'carrot', 'donut', 'grapes', 'hamburger', 
                           'hot-dog', 'ice-cream-cone', 'pear', 'pineapple', 'pizza', 'pumpkin', 'strawberry', 'tomato']
     
-    # Check if all of these subcategories exist in the raw file directory
-    return_folder_not_exist(classes[category], category, images_data_directory)
+    # Check if all of these subclass exist in the raw file directory
+    return_folder_not_exist(classes[subclass], subclass, images_data_directory)
     
-    # Add the following subcategories to the household objects class
+    # Add the following subclass to the household objects class
     classes['household_objects'] = ['alarm clock', 'armchair', 'ashtray', 'bed', 'bookshelf', 'bowl', 'cabinet', 
                                        'candle', 'chair', 'couch', 'door', 'fan', 'floor lamp', 'fork', 'frying-pan', 
                                        'knife', 'ladder', 'tablelamp', 'mug', 'spoon', 'stapler', 'table', 
                                        'teapot', 'toilet', 'toothbrush', 'tv', 'eyeglasses', 'umbrella']
     
-    # Check if all of these subcategories exist in the raw file directory
-    category = 'household_objects'
-    return_folder_not_exist(classes[category], category, images_data_directory)
+    # Check if all of these subclass exist in the raw file directory
+    subclass = 'household_objects'
+    return_folder_not_exist(classes[subclass], subclass, images_data_directory)
     
     # Add the following categories in the miscellaneous categories
-    category = 'miscellaneous'
-    classes[category] = ['binoculars', 'boomerang', 'brain', 'calculator', 'crown', 'diamond', 'envelope', 'present',
+    subclass = 'miscellaneous'
+    classes[subclass] = ['binoculars', 'boomerang', 'brain', 'calculator', 'crown', 'diamond', 'envelope', 'present',
                                     'parachute', 'paper clip', 'human-skeleton', 'sword', 'revolver', 'grenade', 'castle']
     
-    # Check if all of these subcategories exist in the raw file directory
-    return_folder_not_exist(classes[category], category, images_data_directory)
+    # Check if all of these subclass exist in the raw file directory
+    return_folder_not_exist(classes[subclass], subclass, images_data_directory)
 
     # Save the categories to a .json file
     with open(os.path.join(data_directory, 'categories.json'), 'w') as file:
